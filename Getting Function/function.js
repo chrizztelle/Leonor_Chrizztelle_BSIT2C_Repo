3ea0395daf7ff46888e8b08a008622ab694f43d5
chrizztelle leonor <chrizztellel@gmail.com>
2025-03-20 15:41:19 +0800

    battleship v2 & functions

---
+++ Getting Function/function.js
@@ -0,0 +1,43 @@
+// var dogname = "ayel";
+// var dogweight = 18;
+// if (dogweight > 20) {
+//     console.log(dogname + "SAYS WOOF WOOF")
+// }  else {
+//     console.log (dogname + "says woof woof")
+// }
+
+// var dogname = "maggie";
+// var dogweight = 20;
+// if (dogweight > 20) {
+//     console.log(dogname + "SAYS WOOF WOOF")
+// }  else {
+//     console.log (dogname + "says woof woof")
+// }
+
+// var dogname = "alpha";
+// var dogweight = 18;
+// if (dogweight > 20) {
+//     console.log(dogname + "SAYS WOOF WOOF")
+// }  else {
+//     console.log (dogname + "says woof woof")
+// }
+
+// var dogname = "omega";
+// var dogweight = 18;
+// if (dogweight > 20) {
+//     console.log(dogname + "SAYS WOOF WOOF")
+// }  else {
+//     console.log (dogname + "says woof woof")
+// }
+
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + "says woof woof");
+    }
+}
+
+bark("maggie", 20);
+bark("omega", 18);
