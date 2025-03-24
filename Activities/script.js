d9c7155cfd73541b81cb51ed3d74ef8fc7207c5f
chrizztelle <chrizztellel@gmail.com>
2025-03-24 15:15:59 +0800

    function Activity 1

---
+++ Activities/script.js
@@ -0,0 +1,13 @@
+function whatShallIWear (temperature) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } else if (temp < 70){
+        console.log("wear a sweater")
+    } else {
+        console.log("wear a t-shirt")
+    }
+    }
+
+    whatShallIWear(50)
+    whatShallIWear(80)
+    whatShallIWear(60)
